aa216382576d1a98906168bcebd97e2cd7277fc1
Lex Flagel <flag0010@gmail.com>
2016-07-17 14:53:14 -0500

    added track worst time per gen. in green

--- plot.run.stats.py
+++ plot.run.stats.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 from common import get_file
 import sys
-from collections import deque
+from collections import deque, defaultdict
 import itertools
 
 def moving_average(iterable, n=3):
@@ -29,7 +29,7 @@ def yield_N(x, n):
     return k
 
 x = get_file(sys.argv[1])
-
+#for i in x: print i
 etime = [i for i in x if 'elapsed' in i]
 gen = [int(i[0].replace('generation=', '').replace(';', '')) for i in etime]
 sec_cum = [float(i[-1]) for i in etime]
@@ -38,12 +38,34 @@ sec_per = []
 for i in sec_cum:
     sec_per.append(i-last)
     last = i
+
+indv_times = [i for i in x if "run" in i and 'time' in i and '(sec)=' in i]
+qx = [j for j in x if j]
+indvs = [i for i in qx if 'individual=' in i[0]]
+
+g = defaultdict(list)
+
+for i,j in zip(indvs, indv_times):
+    g[i[0]].append(float(j[-1])) 
+
+worst_pos = []
+worst_per_gen = []
+for i in range(len(g[g.keys()[0]])):
+    bad = 0
+    for indv in g:
+        if g[indv][i] > bad: bad = g[indv][i]
+    worst_pos.append( i + 1 )
+    worst_per_gen.append( bad )
+
 mv_ave = list(moving_average(sec_per, n=10))
 plt.subplot(3,1,1)
 plt.plot(gen, sec_per)
 
 plt.ylabel('per generation runtime (sec)')
 plt.plot(range(1, len(mv_ave)+1), mv_ave, color='r')
+
+plt.plot(worst_pos, worst_per_gen, color='g')
+
 
 elite_line = [i for i in x if 'saving' in i][0]
 elite_num = int(elite_line[elite_line.index('best')+1])
@@ -81,3 +103,4 @@ plt.plot(gen, best)
 #print best
 plt.ylabel('fitness of best order (lnLk)')
 plt.show()
+print max(best)
